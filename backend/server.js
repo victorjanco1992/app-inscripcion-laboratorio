9d8366676c84abb40676879b73e0fd16382e4f49
VictorAlejandroJanco <victorjanco1992@gmail.com>
2025-10-31 01:42:43 -0300

    Change server port from 3000 to 1000

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PDFDocument = require('pdfkit');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 
 app.use(cors());
 app.use(express.json());
